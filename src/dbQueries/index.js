39371caebec80462cd0e0cfda574523267805347
Sparsh <nactus123@gmail.com>
2024-08-21 00:40:18 +0530

    Prod db fixes

--- src/dbQueries/index.js
+++ src/dbQueries/index.js
@@ -1,14 +1,23 @@
 import pkg from 'pg';
-import { DB_PASSWORD, DB_PORT, DB_USER, NODE_ENV } from '../env.js';
+import {
+  DB_HOST_PROD,
+  DB_PASSWORD,
+  DB_PASSWORD_PROD,
+  DB_PORT,
+  DB_PORT_PROD,
+  DB_USER,
+  DB_USER_PROD,
+  NODE_ENV
+} from '../env.js';
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: NODE_ENV !== 'production' ? 'localhost' : 'abc',
-  port: DB_PORT,
-  database: 'Nactuz'
+  user: NODE_ENV !== 'production' ? DB_USER : DB_USER_PROD,
+  password: NODE_ENV !== 'production' ? DB_PASSWORD : DB_PASSWORD_PROD,
+  host: NODE_ENV !== 'production' ? 'localhost' : DB_HOST_PROD,
+  port: NODE_ENV !== 'production' ? DB_PORT : DB_PORT_PROD,
+  database: NODE_ENV !== 'production' ? 'Nactuz' : 'postgres'
 });
 
 const query = (text, params) => pool.query(text, params);
--- src/env.js
+++ src/env.js
@@ -6,8 +6,12 @@ if (config) config.config();
 export const {
   NODE_ENV,
   DB_USER,
+  DB_USER_PROD,
   DB_PASSWORD,
+  DB_PASSWORD_PROD,
   DB_PORT,
+  DB_PORT_PROD,
+  DB_HOST_PROD,
   CRYPTO_KEY,
   INITIALIZATION_VECTOR
 } = process.env;
